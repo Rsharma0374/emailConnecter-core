94578c824184b9fc89ffdb1e56422f08ec46e7b3
rahulsharma <rahulsha@lentra.ai>
2024-09-29 18:04:10 +0530

    Removed sms service

--- src/main/java/com/emailConnecter/service/SmsService.java
+++
@@ -1,8 +0,0 @@
-package com.emailConnecter.service;
-
-import com.emailConnecter.request.SmsRequest;
-import com.emailConnecter.response.BaseResponse;
-
-public interface SmsService {
-    BaseResponse sendSms(SmsRequest smsRequest);
-}
