a302e14fd582cddded71249c17d59ccf10cee033
Rahul Sharma <rahulsha@lentra.ai>
2026-09-14 02:01:18 +0530

    removed eureka

--- src/main/java/com/emailConnecter/EmailConnecterCoreApplication.java
+++ src/main/java/com/emailConnecter/EmailConnecterCoreApplication.java
@@ -2,7 +2,6 @@ package com.emailConnecter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Main application class for the Email Connecter Core application.
